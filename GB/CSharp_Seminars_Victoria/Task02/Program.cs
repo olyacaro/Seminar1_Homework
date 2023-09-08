d82872adaa560d660e6073b8cec8c17d738f3788
Olga <olyacaro@mail.ru>
2023-09-09 02:16:35 +0300

    add !!!Есть ошибка в форматировании. Вывести на одной строке три числа от пользователя с одним пробелом между ними.

---
+++ GB/CSharp_Seminars_Victoria/Task02/Program.cs
@@ -0,0 +1,7 @@
+﻿//Вывести на одной строке три числа от пользователя с одним пробелом между ними.
+
+System.Console.WriteLine("Введите три числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Вы ввели числа: {num1} {num2} {num3}");
