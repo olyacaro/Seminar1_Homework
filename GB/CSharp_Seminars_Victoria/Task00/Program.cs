e07ea2305e67e2cdd4b0a1b638211d9b5f3e1d3f
Olga <olyacaro@mail.ru>
2023-09-09 02:04:35 +0300

    add Удалена папка Task01. Код из этой задачи проублирован в папку Task00. Задача из Task00 закоммичена в коде.

--- GB/CSharp_Seminars_Victoria/Task00/Program.cs
+++ GB/CSharp_Seminars_Victoria/Task00/Program.cs
@@ -3,5 +3,10 @@
 Выводимому числу должно предшествовать сообщение «Вы ввели число».
 */
 
-System.Console.WriteLine("Вы ввели число: ");
+/*System.Console.WriteLine("Вы ввели число: ");
+int num = Convert.ToInt32(Console.ReadLine());*/
+
+Console.WriteLine("Введи число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Вы ввели число: "+num);
+Console.WriteLine($"Вы ввели число: {num}");
