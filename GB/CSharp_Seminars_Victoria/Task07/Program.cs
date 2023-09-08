4008548f440e5b0e3619929218451554d043f240
Olga <olyacaro@mail.ru>
2023-09-09 02:57:30 +0300

    add Дорешать и разобраться!!! Дана длина ребра куба. Найти объем куба и площадь его боковой поверхности.

---
+++ GB/CSharp_Seminars_Victoria/Task07/Program.cs
@@ -0,0 +1,7 @@
+﻿//Дана длина ребра куба. Найти объем куба и площадь его боковой поверхности.
+
+/*System.Console.WriteLine("Введите длину ребра куба: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int v = Math.Pow(num, 3) + "куб.см");
+int s = Math.Pow(num, 2) * 6 + "кв.см");
+System.Console.WriteLine($"Объем куба: {v} ");*/
