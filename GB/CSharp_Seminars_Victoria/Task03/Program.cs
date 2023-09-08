3ee0976250b55dfdf364092791ba2f61a89d9dcb
Olga <olyacaro@mail.ru>
2023-09-09 02:22:16 +0300

    add Вывести на экран два числа от пользователя одно под другим.

---
+++ GB/CSharp_Seminars_Victoria/Task03/Program.cs
@@ -0,0 +1,6 @@
+﻿//Вывести на экран два числа от пользователя одно под другим.
+
+System.Console.WriteLine("Введите два числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Число 1: {num1} \nЧисло 2: {num2}");
