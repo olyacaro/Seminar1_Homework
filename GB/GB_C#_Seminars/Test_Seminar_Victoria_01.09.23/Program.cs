6af16f4a8511589ea32b924eec800b050de4e977
Olga <olyacaro@mail.ru>
2023-09-08 16:42:15 +0300

    add Программа, которая на вход принимает число и выдает его квадрат

---
+++ GB/GB_C#_Seminars/Test_Seminar_Victoria_01.09.23/Program.cs
@@ -0,0 +1,13 @@
+﻿// int num = Convert.ToInt32(Console.ReadLine());
+// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное само на себя);
+
+/*Console.WriteLine("Введите число ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+int sqr = num * num;
+Console.WriteLine(sqr);*/
+
+/*Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num * num);*/
+
