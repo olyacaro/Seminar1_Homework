c3c33a65b3de6eb5d770e63ec7cd8238fe88992d
Olga <olyacaro@mail.ru>
2023-09-09 02:40:34 +0300

    add Дана сторона квадрата. Найдите его периметр.

---
+++ GB/CSharp_Seminars_Victoria/Task05/Program.cs
@@ -0,0 +1,6 @@
+﻿/* Дана сторона квадрата. Найти его периметр.*/
+
+System.Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int p = num1*4;
+System.Console.WriteLine($"Периметр квадрата равен: {p} ");
