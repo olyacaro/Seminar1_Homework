261b5e44ab48ee44c733167be554943b314c3174
Olga <olyacaro@mail.ru>
2023-09-09 02:43:04 +0300

    add Дан радиус окружности. Найти ее диаметр.

---
+++ GB/CSharp_Seminars_Victoria/Task06/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Дан радиус окружности. Найти ее диаметр.*/
+
+System.Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+//num1 = num1*2;
+int d = num1*2;
+System.Console.WriteLine($"Диаметр окружности: {d} ");
