c4c8ab44b356d54441bb0a2920e832cf3d55802f
Olga <olyacaro@mail.ru>
2023-09-09 18:03:09 +0300

    add Напишите программу, которая на вход принимает трехзначное число и на выходе показыват последнюю цифру этого числа. 456 -> 6 782 -> 2

---
+++ GB/GB_CSharp_Seminars/Sem1_task7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число
+// и на выходе показыват последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+
+Console.Write("Введи число; ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 99 && num < 1000)
+{
+    int newNum = num % 10;
+    Console.WriteLine(newNum);
+}
+else
+{
+    Console.WriteLine("Неверное число!");
+}
