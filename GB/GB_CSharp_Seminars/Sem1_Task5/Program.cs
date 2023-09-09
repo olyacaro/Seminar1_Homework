505117dffa62f3c4dc2d983cdacdf29600b6b2fe
Olga <olyacaro@mail.ru>
2023-09-09 17:50:00 +0300

    add Напишите программу, которая на вход принимает одно число (N) а на выходе показывает все целые числа от -N до N. 4 -> "-4 -3 -2 -1 0 1 2 3 4" 2 -> "-2 -1 0 1 2"

---
+++ GB/GB_CSharp_Seminars/Sem1_Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает одно число (N)
+// а на выходе показывает все целые числа от -N до N.
+// 4 -> "-4 -3 -2 -1 0 1 2 3 4"
+// 2 -> "-2 -1 0 1 2"
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int numNegative = num * -1;
+while(numNegative <= num)
+{
+    Console.Write(numNegative);
+    Console.Write(", ");
+    numNegative = numNegative + 1;
+}
