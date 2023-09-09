796f52a4fd4d3c604a4b2fbc6e9f9ebb6f4904fe
Olga <olyacaro@mail.ru>
2023-09-09 03:25:18 +0300

    add Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка) 4 -> да -3 -> нет 7 -> нет

---
+++ GB/GB_CSharp_Seminars/Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая на вход принимает число и выдает,
+является ли число четным (делится ли оно на два без остатка)
+4 -> да
+-3 -> нет
+7 -> нет */
+
+Console.WriteLine("Введите число ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+if (userNum == 0)
+    Console.WriteLine("Число равно нулю ");
+else if (userNum%2 == 0)
+    Console.WriteLine("Число четное ");
+else 
+    Console.WriteLine("Число нечетное ");
